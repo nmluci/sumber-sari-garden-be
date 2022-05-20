388eacc7f4f3109123d0c351c99b2fc5083a22ab
nmluci <yryuu@outlook.com>
2022-05-20 22:35:57 +0800

    [feat] finished user profile fetching

--- internal/dto/user_response.go
+++ internal/dto/user_response.go
@@ -15,6 +15,16 @@ type UserSignInResponse struct {
 	AccessToken string `json:"access_token"`
 }
 
+type UserProfileResponse struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Phone     string `json:"phone"`
+	Address   string `json:"address"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	RoleID    int64  `json:"role_id"`
+}
+
 func NewUserSignInResponse(cred *models.UserCred, info *models.UserInfo, ac string) (res *UserSignInResponse, err error) {
 	if cred == nil || info == nil {
 		log.Printf("[NewUserSignInResponse] failed to encode response data due to inconsisted data")
@@ -28,6 +38,26 @@ func NewUserSignInResponse(cred *models.UserCred, info *models.UserInfo, ac stri
 		Email:       cred.Email,
 		RoleID:      cred.UserRole,
 		AccessToken: ac,
+	}
+
+	return
+}
+
+func NewUserProfileResponse(cred *models.UserCred, info *models.UserInfo) (res *UserProfileResponse, err error) {
+	if cred == nil || info == nil {
+		log.Printf("[NewUserProfileResponse] failed to encode response data due to inconsistent data")
+		err = errors.ErrInvalidResources
+		return
+	}
+
+	res = &UserProfileResponse{
+		FirstName: info.FirstName,
+		LastName:  info.LastName,
+		Phone:     info.Phone,
+		Address:   info.Address,
+		Email:     cred.Email,
+		Password:  "Password Hidden",
+		RoleID:    cred.UserRole,
 	}
 
 	return
